37141750b2663bc6b9b0cce366c0c121655a3d6f
igorpcar <igor.pcar@gmail.com>
2025-07-18 15:07:34 -0300

    [nvim] enabled render-markdown

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,7 +1,7 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-	enabled = false,
+	enabled = true,
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
 
